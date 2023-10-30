3b1156c5ed4bc7724244388a27cb8a36b44630d3
Mattie Nash <mnash@kittymail.com>
2023-10-29 23:31:36 -0500

    Commit 1

---
+++ WPILibInstaller-Avalonia/CLI/CLIInstaller.cs
@@ -0,0 +1,8 @@
+namespace WPILibInstaller.CLI;
+
+public class CLIInstaller {
+    public CLIInstaller(string[] args)
+    {
+
+    }
+}
--- WPILibInstaller-Avalonia/Program.cs
+++ WPILibInstaller-Avalonia/Program.cs
@@ -1,5 +1,7 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.ReactiveUI;
+using WPILibInstaller.CLI;
 
 namespace WPILibInstaller
 {
@@ -8,8 +10,17 @@ namespace WPILibInstaller
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Use --help to get command line usage.");
+                BuildAvaloniaApp()
+                    .StartWithClassicDesktopLifetime(args);
+            }
+            else
+                new CLIInstaller(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
